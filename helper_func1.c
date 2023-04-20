adbf3e81be758f45dcaf2812b9e0af3ab2b014f6
IdiJr <idialijnr@gmail.com>
2023-04-20 15:03:36 +0000

    [feature]: added helper functions

---
+++ helper_func1.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+
+/**
+ * _help - Calls help information for the builtin help.
+ * Return: void
+ */
+void _help(void)
+{
+	char *help = "help: help [-dms] [pattern ...]\n";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tDisplay information about builtin commands.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "Displays brief summaries of builtin commands.\n";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
+/**
+ * help_alias - Calls help information for the builtin alias.
+ * Return: void
+ */
+void help_alias(void)
+{
+	char *help = "alias: alias [-p] [name[=value]...]\n";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tDefine or display aliases.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
+/**
+ * help_cd - Calls help information for the builtin cd.
+ * Return: no return
+ */
+void help_cd(void)
+{
+	char *help = "cd: cd [-L|[-P [-e]] [-@]] [dir]\n";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tChange the shell working directory.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
